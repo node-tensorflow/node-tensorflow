cc79b04d976d0cb3470b1f22cb46c3ffa69ef9f2
Peter Braden <peterbraden@peterbraden.co.uk>
2016-02-09 16:38:54 +0100

    no python path

--- tools/build.sh
+++ tools/build.sh
@@ -2,7 +2,7 @@
 cd "lib/tensorflow"
 echo ""
 echo "= Configuring TensorFlow (GPU: $TENSORFLOW_GPU_ENABLED)"
-./configure
+echo "\n" | ./configure
 echo "= Compiling TensorFlow [cc|core]"
 bazel build //tensorflow/cc:cc_ops //tensorflow/core:tensorflow
 
