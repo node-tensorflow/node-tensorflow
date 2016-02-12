5bd46d378cfbc5930bf2585a2ce7340ef9fb7515
Peter Braden <peterbraden@peterbraden.co.uk>
2016-02-12 16:27:27 +0100

    try splitting task

--- tools/build.sh
+++ tools/build.sh
@@ -3,6 +3,9 @@ echo ""
 git reset --hard
 patch -Np1 < ../../tools/tensorflow.patch # From https://github.com/bytedeco/javacpp-presets/blob/master/tensorflow/tensorflow-master.patch
 echo "\n" | ./configure
+
+# Prebuild some deps 
+bazel build --verbose_failures=1 @jpeg_archive//:jpeg
 bazel build --verbose_failures=1 -c opt //tensorflow/cc:libtensorflow.so
 #bazel build --verbose_failures=2 //tensorflow/cc:cc_ops //tensorflow/core:protos_all_cc //tensorflow/core:tensorflow
 
